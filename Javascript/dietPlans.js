377c9e38c3f1994ead3ef97cc013a45e51e8e4a9
MATALAME <141719513+MATALAME@users.noreply.github.com>
2024-09-11 14:19:55 +0200

    First Commit
    Bunch Of Stuff

---
+++ Javascript/dietPlans.js
@@ -0,0 +1,23 @@
+const dietPlan = [
+    {
+        id: 1,
+        name: "Citrus Diet Plan (Basic Plan)",
+        description:"Includes weekly citrus-based meal plans, recipes, nutritional tips, a monthly progress check-in, and one mentor consultation.",
+        price: "R120" + "/pm",
+    },
+
+    {
+        id: 2,
+        name: "Tropical Diet Plan (Premium Plan)",
+        description:"Offers personalized tropical fruit meal plans, shopping lists, two mentor consultations, fitness tips, and access to exclusive online events.",
+        price: "R180" + "/pm",
+    },
+
+    {
+        id: 3,
+        name: "Berry Fruit Diet Plan (Elite Plan)",
+        description:"Features custom berry-based meal plans, daily updates, weekly coaching sessions, unlimited mentor support, and exclusive discounts on berry products.",
+        price: "R220" + "/pm",
+    }
+];
+
---
+++ Javascript/navMenu.js
@@ -0,0 +1,50 @@
+const root = "/WSOA3029A_2538590_Assignment3";
+
+const menuItems = [
+    { name: "Home", href: root + "/index.html"},
+    { name: "Unlock", href: `${root}/Unlock/index.html` },
+    { name: "Design", href: `${root}/Design/index.html` },
+    { name: "Explore", href: `${root}/Explore/index.html` },
+];
+
+
+export function initialise(currentPage) {
+    const nav = document.querySelector("header > nav");
+    const ul = document.createElement("ul");
+    ul.className = "nav__links";
+
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        if (currentPage !== menuItem.name) {
+            a.innerText = menuItem.name;
+            a.setAttribute("href", menuItem.href);
+            li.appendChild(a);
+        } else {
+            li.innerText = menuItem.name;
+            li.classList.add("current-page");
+        }
+
+        ul.appendChild(li);
+    }
+
+    nav.appendChild(ul);
+
+    // Create hamburger icon
+    const hamburger = document.createElement("div");
+    hamburger.classList.add("hamburger");
+    for (let i = 0; i < 3; i++) {
+        const bar = document.createElement("div");
+        hamburger.appendChild(bar);
+    }
+    nav.appendChild(hamburger);
+
+   // Toggle menu visibility on hamburger click
+const menuToggle = document.querySelector(".hamburger");
+menuToggle.addEventListener("click", () => {
+    const menu = document.querySelector(".nav__links");
+    menu.classList.toggle("show");
+});
+
+}
