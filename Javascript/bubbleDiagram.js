19152d44bc9c69975cf5e15ecf637a8215e4883e
MATALAME <141719513+MATALAME@users.noreply.github.com>
2024-09-27 03:00:43 +0200

    Testing proxy

--- Javascript/bubbleDiagram.js
+++ Javascript/bubbleDiagram.js
@@ -1,4 +1,4 @@
-fetch('https://www.fruityvice.com/api/fruit/all')
+fetch('https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit/all')
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok: ' + response.statusText);
